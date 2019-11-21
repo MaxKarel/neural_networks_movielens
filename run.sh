7a608046fc4ce04077718aab5208d4e264670d56
Max Karel <maxdkarel@gmail.com>
2019-11-21 21:50:28 +0100

    docker stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -u $(id -u):$(id -g) -p 8888:8888 -p 6006:6006 -v $(pwd):/labs -it mpikuliak/nsiete
+#docker run --rm -u $(id -u):$(id -g) -p 8888:8888 -p 6006:6006 -v $(pwd):/labs -it mpikuliak/nsiete
+docker run --rm -u $(id -u):$(id -g) -p 8888:8888 -p 6006:6006 -v $(pwd):/labs -it urmegil/movielens
